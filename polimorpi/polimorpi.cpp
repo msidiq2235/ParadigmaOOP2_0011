4b88089e2004eb68459d4831768debfc15afdd76
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-28 17:47:54 +0700

    int main

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -22,3 +22,18 @@ public:
 		cout << "Pesan dari lia" << endl;
 	}
 };
+
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+	
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+	//a.seseorang::pesan();
+
+	return 0;
+
+}
